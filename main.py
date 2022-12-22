11a2001168812749e70f171ea29804c2e1dbb154
zwy923 <811739729@qq.com>
2022-12-22 11:52:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ def move_mouse(x,y):
 
 # Load the YOLOv5 model
 model = yolov5.load('yolov5s.pt')
-# Capture the screen using OpenCV
-screen = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_BGR2RGB)
-# Run object detection on the screen
-predictions = model(screen)
-df=predictions.pandas().xyxy[0].sort_values('confidence',ascending=False)
-# Display the result
-data=df[df['name'] == 'person'].iloc[0].to_dict()
-x=(data['xmin']+data['xmax'])/2
-y=(data['ymin']+data['ymax'])/2
-print(x,y)
-move_mouse(x,y)
+while True:
+    # Capture the screen using OpenCV
+    screen = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_BGR2RGB)
+    # Run object detection on the screen
+    predictions = model(screen)
+    df=predictions.pandas().xyxy[0].sort_values('confidence',ascending=False)
+    # Display the result
+    data=df[df['name'] == 'person'].iloc[0].to_dict()
+    x=(data['xmin']+data['xmax'])/2
+    y=(data['ymin']+data['ymax'])/2
+    print(x,y)
+    move_mouse(x,y)

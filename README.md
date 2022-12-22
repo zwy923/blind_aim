ab0f41b080741cd1826865b91c7b9cb711006352
zwy923 <811739729@qq.com>
2022-12-22 11:48:14 +0200

    12.22
    read the location

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # blind_aim
+
+1.训练yolov5模型
+
+2.opencv截图屏幕图像
+
+3.识别图像中的对象
+
+4.移动鼠标至图像坐标
+
 Fps aimbot by yolov5
---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+from PIL import ImageGrab
+import yolov5
+import pyautogui
+import threading
+
+#移动鼠标位置
+def move_mouse(x,y):
+    pyautogui.moveTo(x,y)
+
+# Load the YOLOv5 model
+model = yolov5.load('yolov5s.pt')
+# Capture the screen using OpenCV
+screen = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_BGR2RGB)
+# Run object detection on the screen
+predictions = model(screen)
+df=predictions.pandas().xyxy[0].sort_values('confidence',ascending=False)
+# Display the result
+data=df[df['name'] == 'person'].iloc[0].to_dict()
+x=(data['xmin']+data['xmax'])/2
+y=(data['ymin']+data['ymax'])/2
+print(x,y)
+move_mouse(x,y)

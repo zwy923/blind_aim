3ed4a332a4ca39b8377a4b2c6c568ee57e023bad
zwy923 <811739729@qq.com>
2022-12-24 10:52:37 +0200

    Update pid.py

--- pid.py
+++ pid.py
@@ -1,30 +1,28 @@
 class PID:
-    def __init__(self, dt, max, min, Kp, Ki, Kd):
-        self.dt = dt  # 循环时长
-        self.max = max  # 操作变量最大值
-        self.min = min  # 操作变量最小值
-        self.Kp = Kp  # 比例增益
-        self.Ki = Ki  # 积分增益
-        self.Kd = Kd  # 微分增益
-        self.integral = 0  # 直到上一次的误差值
-        self.pre_error = 0  # 上一次的误差值
+    def __init__(self, dt, max, min, p, i, d):
+        self.dt = dt
+        self.max = max
+        self.min = min
+        self.Kp = p  
+        self.Ki = i 
+        self.Kd = d  
+        self.integral = 0  
+        self.pre_error = 0  
 
     def calculate(self, setPoint, pv):
-        # 其中 pv:process value 即过程值，
-        error = setPoint - pv  # 误差
-        Pout = self.Kp * error  # 比例项
+        error = setPoint - pv 
+        Pout = self.Kp * error 
         self.integral += error * self.dt
-        Iout = self.Ki * self.integral  # 积分项
+        Iout = self.Ki * self.integral
         derivative = (error - self.pre_error) / self.dt
-        Dout = self.Kd * derivative  # 微分项
-
-        output = Pout + Iout + Dout  # 新的目标值
+        Dout = self.Kd * derivative
+        output = Pout + Iout + Dout
 
         if (output > self.max):
             output = self.max
         elif (output < self.min):
             output = self.min
 
-        self.pre_error = error  # 保存本次误差，以供下次计算
+        self.pre_error = error
         return output
 
